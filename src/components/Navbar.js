e95c0792d98e8271bd1c703c07ce2e412a22f10f
ALVARO P <alv.gomez@outlook.com>
2021-09-14 10:55:26 +0200

    Create Slider and Animation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Search, ShoppingCartOutlined } from "@material-ui/icons";
 import styled from "styled-components";
 
 const Container = styled.div`
-	height: 60px;
+	height: 100px;
 `;
 
 const Wrapper = styled.div`
