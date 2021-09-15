0ea0cec9995c75e8cb64e190391c6f98090a76ab
ALVARO P <alv.gomez@outlook.com>
2021-09-15 12:14:47 +0200

    Add Popular Products List to Home Component

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,26 +5,66 @@ import {
 	ShoppingCartOutlined,
 } from "@material-ui/icons";
 
+const Info = styled.div`
+	width: 100%;
+	height: 100%;
+	background-color: rgba(0, 0, 0, 0.1);
+	z-index: 3;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	position: absolute;
+	top: 0;
+	left: 0;
+	opacity: 0;
+	transition: all 0.3s ease;
+`;
+
 const Container = styled.div`
 	flex: 1;
 	margin: 5px;
 	min-width: 280px;
 	height: 350px;
 	display: flex;
-	align-item: center;
+	align-items: center;
 	justify-content: center;
 	background-color: #f5fbfd;
+	position: relative;
+
+	&:hover ${Info} {
+		opacity: 1;
+	}
 `;
 
-const Circle = styled.div``;
+const Circle = styled.div`
+	width: 200px;
+	height: 200px;
+	border-radius: 50%;
+	background-color: white;
+	position: absolute;
+`;
 
 const Image = styled.img`
 	height: 75%;
+	z-index: 2;
 `;
 
-const Info = styled.p``;
+const Icon = styled.div`
+	width: 40px;
+	height: 40px;
+	border-radius: 50%;
+	background-color: white;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	margin: 10px;
+	transition: all 0.3s ease;
 
-const Icon = styled.div``;
+	&:hover {
+		background-color: #e9f5f5;
+		transform: scale(1.1);
+	}
+`;
 
 const Product = ({ item }) => {
 	return (
