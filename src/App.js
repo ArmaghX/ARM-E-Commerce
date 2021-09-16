e84f1bfa7b6e534a2fa14b819ffd1013436eb13c
ALVARO P <alv.gomez@outlook.com>
2021-09-16 20:49:45 +0200

    Create Cart Component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import Home from "./screens/Home";
 import ProductList from "./screens/ProductList";
 import Register from "./screens/Register";
 import Login from "./screens/Login";
+import Cart from "./screens/Cart";
 
 function App() {
-	return <Login />;
+	return <Cart />;
 }
 
 export default App;
